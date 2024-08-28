fcc42d7e896f2fee3d9d6d95d71ba4b3c782f26d
Bagus Gandhi <bagus.gandhi@datasintesa.id>
2024-08-28 14:09:55 +0700

    feat: common utils env loader

---
+++ src/common/env.ts
@@ -0,0 +1,12 @@
+import getConfig from 'next/config';
+
+// const { serverRuntimeConfig } = getConfig();
+
+export const Env = {
+    // GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID as string,
+    // GOOGLE_SECRET: process.env.GOOGLE_SECRET as string,
+    // SOCKET_URL: process.env.SOCKET_URL as string,
+    BE_API_HOST: process.env.BE_API_HOST as string,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET as string,
+    SECRET_API_KEY: process.env.SECRET_API_KEY as string,
+}
