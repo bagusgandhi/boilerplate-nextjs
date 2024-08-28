1acd817a8fdbba2cf695140b3af7bc362e23c4eb
Bagus Gandhi <bagus.gandhi@datasintesa.id>
2024-08-28 14:00:59 +0700

    feat: add hooks swr fetcher

---
+++ src/utils/hooks/useSwrFetcher.ts
@@ -0,0 +1,52 @@
+import useSWR, { SWRConfiguration } from 'swr';
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+import { notification } from 'antd';
+import { signOut } from 'next-auth/react'; // Import signOut from next-auth
+
+interface UseSWRFetcherProps {
+  key: string | [string, AxiosRequestConfig?] | undefined;
+  session?: { data?: { accessToken?: string } };
+  swrOptions?: SWRConfiguration;
+  axiosOptions?: AxiosRequestConfig;
+  allRes?: boolean;
+}
+
+export function useSWRFetcher<T>({
+  key,
+  session,
+  swrOptions,
+  axiosOptions,
+  allRes = false,
+}: UseSWRFetcherProps) {
+  return useSWR<T>(
+    key,
+    async (key) => {
+      const url = Array.isArray(key) ? key[0] : key;
+      const axiosConfig: AxiosRequestConfig = {
+        url,
+        baseURL: process.env.NEXT_PUBLIC_API_URL,
+        // headers: { Authorization: `Bearer ${session?.data?.accessToken}` }, // Uncomment to include the access token
+        // ...((typeof key === 'object' && !Array.isArray(key) && key !== null) && key),
+        ...axiosOptions,
+      };
+      const response: AxiosResponse = await axios(axiosConfig);
+      return allRes ? response : response.data;
+    },
+    {
+      revalidateOnFocus: false,
+      shouldRetryOnError: false,
+      onError: (err) => {
+        if (err?.response?.status === 401) {
+          // Trigger sign-out when a 401 status is detected
+          signOut({ callbackUrl: '/auth' }); // Redirect to the login page after sign-out
+        } else {
+          notification['error']({
+            message: err?.response?.data?.error ?? 'Error!',
+            description: err?.response?.data?.message ?? err?.message,
+          });
+        }
+      },
+      ...swrOptions,
+    }
+  );
+}
