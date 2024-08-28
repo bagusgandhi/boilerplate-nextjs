e5febe1815da1d49a9e0e3cfbd5e13e1401dafd2
Bagus Gandhi <bagus.gandhi@datasintesa.id>
2024-08-28 14:02:53 +0700

    feat: add table list user component

---
+++ src/components/ManageUsers/Table/TableListUsers.tsx
@@ -0,0 +1,180 @@
+"use client";
+import { Button, Popconfirm, Spin, Table, notification } from "antd";
+import React, { useContext, useEffect } from "react";
+import { ManageUsersContext } from "../Pages/Users/Index";
+import { DeleteOutlined, EditOutlined } from "@ant-design/icons";
+import { useSWRFetcher } from "@/utils/hooks/useSwrFetcher";
+import { useSWRMutationFetcher } from "@/utils/hooks/useSweFetcherMutation";
+import { useHasPermission } from "@/utils/hooks/usePermission";
+
+export default function TableListUsers({ handlersModal }: any) {
+  const {
+    state: [state, dispatch],
+    resTable,
+    session
+  }: any = useContext(ManageUsersContext);
+
+  const canUpdateUser = useHasPermission({
+    requiredPermission: ["userManagement.updateUser"],
+    session,
+  });
+
+  const canDeleteUser = useHasPermission({
+    requiredPermission: ["userManagement.deleteUser"],
+    session,
+  });
+
+  const deleteUser = useSWRMutationFetcher({
+    key: [`delete:api/user`],
+    axiosOptions: {
+      method: "DELETE",
+      url: `api/user/${state.userId}`,
+    },
+    swrOptions: {
+      onSuccess: (data: any) => {
+        notification["success"]({
+          message: "Success",
+          description: "User has been deleted",
+        });
+      },
+    },
+  });
+
+  const { data: dataRoles, isLoading: isLoadingRoles } = useSWRFetcher<any>({
+    key: [`api/role?viewAll=true`],
+    axiosOptions: {
+      url: "api/role",
+      params: {
+        viewAll: true,
+      },
+    },
+  });
+
+  const filterRole: any[] = dataRoles?.map((role: any) => ({
+    text: role.name,
+    value: role.id,
+  }));
+
+  const columns = [
+    {
+      title: "No",
+      key: "no",
+      width: 50,
+      render: (text: any, record: any, index: number) =>
+        state.pagination.limit * (state.pagination.page - 1) + index + 1,
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Role",
+      dataIndex: "roles",
+      key: "roles",
+      filters: filterRole,
+      render: (record: any) => {
+        return record?.[0]?.name ?? "-";
+      },
+    },
+    {
+      title: "Action",
+      key: "action",
+      width: 400,
+
+      render: (record: any) => {
+        return (
+          <div className="flex gap-2">
+            {canUpdateUser &&(<Button
+              onClick={() => {
+                dispatch({
+                  type: "set formType",
+                  payload: "edit",
+                });
+                dispatch({
+                  type: "set userId",
+                  payload: record.id,
+                });
+                handlersModal.open();
+              }}
+              size="small"
+              icon={<EditOutlined />}
+            >
+              Edit
+            </Button>)}
+            {canDeleteUser && (<Popconfirm
+              placement="bottom"
+              title={"Warning"}
+              description={'Are you sure to delete "' + record.name + '" ?'}
+              okText="Yes"
+              cancelText="No"
+              onConfirm={async () => {
+                await deleteUser.trigger();
+                await resTable.mutate();
+              }}
+            >
+              <Button
+                onClick={() => {
+                  dispatch({
+                    type: "set userId",
+                    payload: record.id,
+                  });
+                }}
+                danger
+                size="small"
+                icon={<DeleteOutlined />}
+              >
+                Delete
+              </Button>
+            </Popconfirm>)}
+          </div>
+        );
+      },
+    },
+  ];
+
+  useEffect(() => {
+    resTable.mutate();
+  }, [state.filter.roleId]);
+
+  return (
+    <>
+      <Spin size="large" spinning={resTable?.isLoading}>
+        <Table
+          dataSource={resTable?.data?.results?.map((item: any) => ({
+            ...item,
+            key: item.id,
+          }))}
+          columns={columns}
+          onChange={(pagination, filters, sorter, extra) => {
+            dispatch({
+              type: "set filter.roleId",
+              payload: filters.roles
+            });
+
+          }}
+          pagination={{
+            current: state.pagination.page,
+            pageSize: state.pagination.limit,
+            total: resTable?.data?.total,
+            position: ["none", "bottomCenter"],
+            onChange: (page, pageSize) => {
+              dispatch({
+                type: "set pagination",
+                payload: {
+                  limit: pageSize,
+                  page,
+                },
+              });
+            },
+          }}
+        />
+      </Spin>
+    </>
+  );
+}
