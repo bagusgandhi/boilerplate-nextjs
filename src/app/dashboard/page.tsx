f85acc3860085757d1f8ad5c3311c0f56e4278d3
Bagus Gandhi Pratama <35498095+bagusgandhi@users.noreply.github.com>
2024-12-08 16:36:09 +0700

    feat: component chart and table

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,24 +2,26 @@ import { getServerSession } from "next-auth/next";
 import React from "react";
 import { authOptions } from "@/lib/authOptions";
 import Title from "antd/es/typography/Title";
-import Test from "@/components/Test";
 import { Breadcrumb } from "antd";
+import DashboardPage from "@/components/Dashboard/Pages/Index";
 
 export default async function page() {
   const session = await getServerSession(authOptions);
+
   return (
     <>
       <div className="bg-white border-b-2 border-gray-100 px-4 py-2">
         <Breadcrumb
-          items={[
-            { title: "Dashboard", href: "/dashboard" }
-          ]}
+          items={[{ title: "Dashboard", href: "/dashboard" }]}
           style={{ margin: "16px 0" }}
         />
         <Title level={4}>Dashboard</Title>
       </div>
 
-      
+      <div className="px-8">
+        <DashboardPage session={session} />
+      </div>
     </>
   );
 }
+
